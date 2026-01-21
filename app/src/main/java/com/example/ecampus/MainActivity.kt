1366ddf595837b6a604c0ede0f6cdfb53a7113b2
serlyagustina <agustinaserly4@mail.com>
2026-01-21 14:00:44 +0700

    tugas

---
+++ app/src/main/java/com/example/ecampus/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.ecampus
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://ecampus.utmj.ac.id")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
